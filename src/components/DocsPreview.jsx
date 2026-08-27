534f36c93c54d9bda58a9544d724064b7a0a6a47
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-08-27 10:19:38 +0530

    fix: adjust border styling for improved responsiveness in DocsPreview component

--- src/components/DocsPreview.jsx
+++ src/components/DocsPreview.jsx
@@ -62,7 +62,7 @@ export default function DocsPreviewSection() {
             return (
               <div
                 key={index}
-                className="bg-slate-950/90 py-5 px-10 rounded-4xl border-3 border-white/10 hover:border-cyan-500/30 transition-all">
+                className="bg-slate-950/90 py-5 px-10 rounded-4xl border-3 md:border-2 border-white/10 hover:border-cyan-500/30 transition-all">
                 <div className="w-10 h-10 rounded-xl bg-cyan-500/10 text-cyan-400 flex items-center justify-center mb-4 border border-cyan-500/20">
                   <Icon className="w-5 h-5" />
                 </div>
