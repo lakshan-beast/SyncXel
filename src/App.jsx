55f21bfdc7d64361eef496c0c8b0a605eaf45b03
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-09-04 09:49:44 +0530

    feat: Comment out SocialProof component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ export default function App() {
                     <TrustBar />
                     <Components />
                     <DocsPreviewSection />
-                    <SocialProof />
+                    {/* <SocialProof /> */}
                     <FeaturesSection />
                     <TeamSection />
                     <ServicesSection />
