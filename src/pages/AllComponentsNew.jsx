21544f72bfdf2d3eba6a3881a0b5cbddfa8c07d3
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-09-17 09:46:32 +0530

    feat: remove commented-out code and clean up TechStack, TrustBar, WhyChoose, and AllComponents files

--- src/pages/AllComponentsNew.jsx
+++ src/pages/AllComponentsNew.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import {
   HiArrowLeft,
-  // HiCheck,
   HiCode,
   HiEye,
   HiExternalLink,
@@ -10,17 +9,12 @@ import {
   HiSparkles,
   HiChatAlt2,
   HiBookOpen,
-  // HiCheckCircle,
-  // HiDuplicate,
 } from "react-icons/hi";
 // import { LuStamp } from "react-icons/lu";
 import { TbFileDownloadFilled } from "react-icons/tb";
 
 import { TbCopyPlusFilled, TbCopyCheckFilled } from "react-icons/tb";
-// import { GiFishBucket } from "react-icons/gi";
-{
-  /* <GiFishBucket /> */
-}
+
 import { GiBoatFishing } from "react-icons/gi";
 
 import { freeComponentsData } from "../data/free/freeIndex";
@@ -95,15 +89,6 @@ export default function AllComponents() {
             <strong className="text-cyan-400">September 2026</strong>
           </span>
         </div>
-
-        {/* Friendly Free Usage Banner */}
-        {/* <div className="bg-cyan-500/10 border border-cyan-500/20 px-4 py-2 rounded-xl flex items-center space-x-2 text-xs text-cyan-300">
-          <HiSparkles className="w-4 h-4 text-cyan-400 shrink-0" />
-          <span>
-            All free components can be used anywhere in your personal & client
-            projects freely!
-          </span>
-        </div> */}
 
         {/* Friendly Free Usage Banner */}
         <div className="bg-red-500/20 ng-blur-md border border-red-500/20 px-4 py-2 mt-2 rounded-xl flex items-center space-x-2 text-xs text-orange-300 text-center ">
@@ -191,92 +176,6 @@ export default function AllComponents() {
               const isCopied = copiedId === item.id;
 
               return (
-                // <div
-                //   key={item.id}
-                //   className="bg-white/15 backdrop-blur-lg  border border-slate-800 rounded-2xl overflow-hidden flex flex-col justify-between shadow-xl">
-                //   {/* Card Header */}
-                //   <div className="px-4 py-6 border-b border-slate-800 bg-slate-900/60 flex items-start justify-between">
-                //     <div>
-                //       <h3 className="text-sm font-bold text-white mb-1">
-                //         {item.title}
-                //       </h3>
-                //       <span className="text-[10px] text-cyan-400 uppercase tracking-wider bg-cyan-400/10 px-4 py-0.5 rounded-full border border-cyan-400/20">
-                //         {item.category}
-                //       </span>
-                //       <p className="text-xs text-slate-400 mt-1.5 mb-0.5 border-t border-white/10 pt-1">
-                //         {item.description}
-                //       </p>
-                //     </div>
-                //     <span className="text-[10px] font-bold text-emerald-400 bg-emerald-400/10 px-2 py-0.5 rounded-md border border-emerald-400/20">
-                //       Free
-                //     </span>
-                //   </div>
-
-                //   {/* Sub-Tabs (Preview vs Code) */}
-                //   <div className="flex px-2 py-2 justify-between ">
-                //     <div className="flex space-x-2">
-                //       <button
-                //         onClick={() => toggleCardTab(item.id, "preview")}
-                //         className={`text-xs px-3 py-1 rounded-lg font-medium transition-all flex items-center space-x-1 cursor-pointer ${currentTab === "preview" ? "bg-cyan-500 text-slate-950 font-bold" : "text-slate-400 hover:text-white"}`}>
-                //         <HiEye className="w-3.5 h-3.5" />
-                //         <span>Preview</span>
-                //       </button>
-                //       <button
-                //         onClick={() => toggleCardTab(item.id, "code")}
-                //         className={`text-xs px-3 py-1 rounded-lg font-medium transition-all flex items-center space-x-1 cursor-pointer ${currentTab === "code" ? "bg-cyan-500 text-slate-950 font-bold" : "text-slate-400 hover:text-white"}`}>
-                //         <HiCode className="w-3.5 h-3.5" />
-                //         <span>Code</span>
-                //       </button>
-                //     </div>
-
-                //     <span className="flex items-center gap-2 font-exo text-xs font-bold text-orange-600 bg-orange-400/10 px-4 py-1 rounded-full border border-orange-400/20  ">
-                //       <TbFileDownloadFilled /> {item.copiesCount}
-                //     </span>
-                //   </div>
-
-                //   {/* Body Content */}
-                //   <div className="p-2 min-h-50 bg-slate-950/60 flex items-center justify-center">
-                //     {currentTab === "preview" ? (
-                //       <div className="text-center w-full flex items-center justify-center">
-                //         {/* අර පරණ item.id කන්ඩිෂන් ඔක්කොම අයින් කරලා මේක විතරක් දාන්න */}
-                //         {/* <div dangerouslySetInnerHTML={{ __html: item.code }} /> */}
-                //         {item.component}
-                //       </div>
-                //     ) : (
-                //       <pre className="w-full h-32 overflow-auto text-[11px] text-cyan-300 bg-slate-950 p-3 rounded-lg border border-slate-800 font-mono text-left">
-                //         <code>{item.code}</code>
-                //       </pre>
-                //     )}
-                //   </div>
-
-                //   {/* Footer Action with Dynamic "Copied! ✨" feedback */}
-                //   <div className="py-2 px-3 bg-slate-900/80 flex items-center justify-between border-t border-slate-800">
-                //     <span className="text-[10px] text-slate-400">
-                //       Ready to copy
-                //     </span>
-
-                //     <button
-                //       onClick={() => handleCopy(item.id, item.code)}
-                //       className={`px-3.5 py-1.5 rounded-lg text-xs font-bold transition-all cursor-pointer flex items-center space-x-1 ${
-                //         isCopied
-                //           ? "bg-emerald-500 text-slate-950 shadow-lg shadow-emerald-500/20"
-                //           : "bg-cyan-500 hover:bg-cyan-400 text-slate-950 shadow-lg shadow-cyan-500/20"
-                //       }`}>
-                //       {isCopied ? (
-                //         <>
-                //           <HiCheck className="w-3.5 h-3.5" />
-                //           <span>Copied!</span>
-                //         </>
-                //       ) : (
-                //         <>
-                //           <HiCheck className="w-3.5 h-3.5" />
-                //           <span>Copy Code</span>
-                //         </>
-                //       )}
-                //     </button>
-                //   </div>
-                // </div>
-
                 // 2nd style
                 <motion.div
                   key={item.id}
