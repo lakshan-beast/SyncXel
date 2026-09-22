72cb777302cef0d39f81794389864e0faed4041e
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-09-22 10:57:49 +0530

    feat: Update NavBar component for improved styling and animation effects

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -37,7 +37,7 @@ export default function NavBar() {
       transition={{ duration: 0.6, ease: "easeOut" }}
       className="fixed top-0 left-0 right-0 px-3 sm:px-8 lg:px-0 pt-3 lg:pt-0 z-50 ">
       {/* Nav Container - Clean White & Black DevSync Style */}
-      <nav className="w-full max-w-7xl lg:max-w-full mx-auto bg-white/90 backdrop-blur-sm border border-slate-300/50 lg:border-none rounded-3xl md:rounded-b-2xl md:rounded-t-none shadow-none lg:shadow-xs transition-all duration-300 relative text-slate-900 px-3 py-2 lg:px-40 lg:pt-2">
+      <nav className="w-full max-w-7xl lg:max-w-full mx-auto bg-white/90 backdrop-blur-sm border border-slate-300/50 lg:border-none rounded-3xl md:rounded-b-2xl md:rounded-t-none shadow-none lg:shadow-xs transition-all duration-300 relative text-slate-900 px-3 py-2 lg:px-30 lg:pt-2">
         {/* logo */}
         <div className="px-4 sm:px-6 py-4 lg:py-5 flex items-center justify-between">
           {/* Logo Section */}
@@ -69,9 +69,13 @@ export default function NavBar() {
                 <span className="text-2xl md:text-3xl font-black font-baloo uppercase bg-transparent text-slate-950 p-0 rounded-l-md shadow-inner leading-0">
                   Sync
                 </span>
-                <span className="text-2xl md:text-3xl font-black font-baloo uppercase bg-slate-950 text-white px-2 py-0.5  ml-0.5 rounded-md border border-l-0 border-white/20 shadow-md leading-none">
+                <motion.span
+                  initial={{ x: -50, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ duration: 0.6, ease: "easeOut" }}
+                  className="text-2xl md:text-3xl font-black font-baloo uppercase bg-slate-950 text-white px-2 py-0.5  ml-0.5 rounded-md border border-l-0 border-white/20 shadow-md leading-none">
                   Xel
-                </span>
+                </motion.span>
               </div>
 
               <span className="text-[10px] font-baloo tracking-widest text-slate-400 -mt-0.5 uppercase">
